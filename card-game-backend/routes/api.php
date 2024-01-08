6943538fbee5d0efb79432c93ef66787d36302bb
Shahriar Saleh Fahim <shahriarfahim19@gmail.com>
2024-01-08 15:30:50 +0600

    added sanctum

--- card-game-backend/routes/api.php
+++ card-game-backend/routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('register', function (Request $request) {
+    return 'hello';
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
